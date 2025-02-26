fb2d60216f6cc9b642c1dfd5334efa1bd089c69e
Yared Deribe <149392861+yaredbderibe@users.noreply.github.com>
2025-02-26 13:31:20 +0300

    Update Week-2 Task.py

--- Week-2/Week-2 Task.py
+++ Week-2/Week-2 Task.py
@@ -8,3 +8,14 @@ print( "And here are the even numbers from 0 to 20: " , end="" )
 for i in range(1,20,1):
     if i % 2 == 0:
         print( i , end=" ")
+
+
+numbers = [1, 2, 10, 3, 4, 5, 6, 7, 8, 9]
+
+x = numbers[0] 
+
+for j in numbers:
+    if j > x:  
+        x = j  
+
+print("The largest number from the list is", x)
